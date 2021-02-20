cc77c386e7928880b7ecbfe200b6ee4a9e821be7
nabil-berjaoui <nabilberjaoui@gmail.com>
2021-02-20 17:46:18 -0500

    Image capture

--- src/Image.py
+++ src/Image.py
@@ -1,11 +1,30 @@
+import cv2
 
-from cv2 import *
-# initialize the camera
-cam = VideoCapture(0)   # 0 -> index of camera
-s, img = cam.read()
-if s:    # frame captured without any errors
-    namedWindow("cam-test");
-    imshow("cam-test",img)
-    waitKey(0)
-    #destroyWindow("cam-test")
-    imwrite("filename.jpg",img) #save image
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("Camera")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()
