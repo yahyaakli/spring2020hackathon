831d5640046a5ec3c68d68042f9c24fb1c673ecd
nabil-berjaoui <nabilberjaoui@gmail.com>
2021-02-20 21:27:43 -0500

    Image capture

--- src/Image.py
+++ src/Image.py
@@ -1,30 +1,63 @@
 import cv2
+import io
+import os
 
-cam = cv2.VideoCapture(0)
+from google.cloud import vision
 
-cv2.namedWindow("Camera")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../uploads/hackathon-d17c6f5e9534.json"
 
-img_counter = 0
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
 
-while True:
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
-        break
-    cv2.imshow("test", frame)
+class Image():
+    def __init__(self):
+        self.labels = []
+        self.image = None
 
-    k = cv2.waitKey(1)
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
+    def capture(self, image):
+        # chi 7aja
+        cam = cv2.VideoCapture(0)
+        cv2.namedWindow("Camera")
+        img_counter = 0
+        while True:
+            ret, frame = cam.read()
+            if not ret:
+                print("failed to grab frame")
+                break
+            cv2.imshow("test", frame)
 
-cam.release()
+            k = cv2.waitKey(1)
+            if k % 256 == 27:
+                # ESC pressed
+                print("Escape hit, closing...")
+                break
+            elif k % 256 == 32:
+                # SPACE pressed
+                img_name = "opencv_frame_{}.png".format(img_counter)
+                cv2.imwrite(img_name, frame)
+                print("{} written!".format(img_name))
+                img_counter += 1
+        cam.release()
+        cv2.destroyAllWindows()
+        self.image = img_name
 
-cv2.destroyAllWindows()
+    def findLabels(self, im):
+        # The name of the image file to annotate
+        file_name = os.path.abspath(self.im)
+
+        # Loads the image into memory
+        with io.open(file_name, 'rb') as image_file:
+            content = image_file.read()
+
+        image = vision.Image(content=content)
+
+        # Performs label detection on the image file
+        response = client.label_detection(image=image)
+        labels = response.label_annotations
+
+        print('Labels:')
+        for label in labels:
+            print(label.description+', score: '+str(label.score))
+
+
+
--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ import io
 import os
 
 from google.cloud import vision
-
+from PIL import Image
+import glob
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="../uploads/hackathon-d17c6f5e9534.json"
 
 # Instantiates a client
@@ -10,13 +11,11 @@ client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
 
-file_name = 'C:/Users/Yahya/Documents/git/spring2020hackathon/uploads/1564684055231.jpeg'
+file_name = os.path.abspath(r"C:\Users\Nabil Berjaoui\Documents\spring2020hackathon\uploads\istockphoto-1163967091-1024x1024.jpg")
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
     content = image_file.read()
-
-image = vision.Image(content=content)
 
 image = vision.Image(content=content)
 
@@ -26,4 +25,4 @@ labels = response.label_annotations
 
 print('Labels:')
 for label in labels:
-    print(label.description)
+    print(label.description+str(label.score))
